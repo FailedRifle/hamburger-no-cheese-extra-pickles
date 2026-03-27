539669ec69ece9fb96ed2fdc68345d00459ea705
FailedRifle <139413504+FailedRifle@users.noreply.github.com>
2026-03-27 13:55:47 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,9 @@ const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./blocked.png"
+  "./blocked.png",
+  "./ched-192.png",
+  "./ched-512.png"
 ];
 
 self.addEventListener("install", event => {
@@ -18,7 +20,9 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
     )
   );
   self.clients.claim();
@@ -28,8 +32,10 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request)
       .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        if (res && res.status === 200 && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
         return res;
       })
       .catch(() => caches.match(event.request))
